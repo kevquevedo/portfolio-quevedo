40388973c2eca503a04d5e2542b9860570f1e02c
Kevin Quevedo <quevedo.kevin@hotmail.com>
2026-04-25 21:25:52 -0300

    arreglo de meta para linkedin

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -15,7 +15,7 @@ import { PortfolioService } from '../../core/services/portfolio';
 export class ProjectsComponent {
   private portfolioService = inject(PortfolioService);
 
-  categories = ['todos', 'backend', 'fullstack'];
+  categories = ['todos'];
   activeFilter = signal('todos');
 
   filteredProjects = computed(() => {
